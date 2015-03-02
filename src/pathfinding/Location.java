5d79541b15e751082a9ab2ce85138b37912b0795
Viktor Holmgren <vikho394@student.liu.se>
2015-03-02 13:44:59 +0100

    create base class for holding x,y coordinates

---
+++ src/pathfinding/Location.java
@@ -0,0 +1,22 @@
+package pathfinding;
+
+/**
+ * Created by Holmgr 2015-03-02
+ * Base datatype containing a pair x,y coordinates which is immutable.
+ */
+public class Location {
+    public final int x,y;
+
+    public Location(final int x, final int y) {
+	this.x = x;
+	this.y = y;
+    }
+
+    @Override public int hashCode() {
+	return 10000 * y + x; // Unique due to x,y never exceeding 10000
+    }
+
+    @Override public boolean equals(final Object obj) {
+	return hashCode() == obj.hashCode();
+    }
+}
