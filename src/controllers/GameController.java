b966969097fb5bfa5f91e4b58c1a1df4227bf8f1
Viktor Holmgren <vikho394@student.liu.se>
2015-03-09 15:34:59 +0100

    implement timer and placeholder enemies

--- src/controllers/GameController.java
+++ src/controllers/GameController.java
@@ -1,11 +1,19 @@
 package controllers;
 
+import enemies.AbstractEnemy;
+import enemies.Enemy;
+import enemies.GroundEnemy;
 import gui.GameComponent;
 import gui.GameFrame;
 import pathfinding.AStarSearch;
 import pathfinding.Location;
 import pathfinding.Path;
 import pathfinding.SquareGrid;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 /**
  * Created by Holmgr 2015-03-09
@@ -21,17 +29,35 @@ public class GameController {
     private Location defaultStart = new Location(0, 0);
     private Location defaultEnd = new Location(8, 11);
 
+    ArrayList<Enemy> enemies;
+
     public GameController() {
 
         grid = createArbitaryGrid();
-        component = new GameComponent(grid, gridSize);
-        frame = new GameFrame(component);
 
         AStarSearch search = new AStarSearch(grid, defaultStart, defaultEnd);
         Path path = search.createPath();
 
+        enemies = new ArrayList<>();
+        enemies.add(new GroundEnemy(path));
+
+        component = new GameComponent(grid, gridSize, enemies);
+        frame = new GameFrame(component);
+
         component.setPath(path);
-        component.repaint();
+
+        Timer loopTimer = new Timer(34, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for(Enemy enemy : enemies){
+                    enemy.moveStep();
+                    component.repaint();
+                }
+            }
+        });
+
+        loopTimer.setCoalesce(true);
+        loopTimer.start();
 
     }
 
