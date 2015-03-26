195cd416a6712de760dcd1c11215d11576f85426
Christoffer <chsnot@gmail.com>
2015-03-26 17:15:49 +0100

    constructor loads file if exists otherwise creates new list

--- src/score/HighscoreList.java
+++ src/score/HighscoreList.java
@@ -5,6 +5,7 @@ import controllers.GameConstants;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,7 +16,16 @@ public class HighscoreList {
     ArrayList<HighscoreEntry> scoreList;
 
     public HighscoreList() {
-        this.scoreList = new ArrayList<>();
+        File f = new File(GameConstants.HIGHSCORE_PATH);
+        if (f.exists() && !f.isDirectory()){
+            System.out.println("Loading file");
+            loadFromFile();
+        }
+        else {
+            System.out.println("No file, creating new list");
+            this.scoreList = new ArrayList<>();
+        }
+
     }
 
     public void add(String name, int score)  {
@@ -33,15 +43,40 @@ public class HighscoreList {
         //serialize the List
         try {
             OutputStream file = new FileOutputStream(GameConstants.HIGHSCORE_PATH);
-            System.out.println(file.toString());
-            OutputStream buffer = new BufferedOutputStream(file);
-            ObjectOutput output = new ObjectOutputStream(buffer);
+            //OutputStream buffer = new BufferedOutputStream(file);
+            ObjectOutput output = new ObjectOutputStream(file);
             output.writeObject(scoreList);
         }
         catch(IOException ex) {
             //Logger.log(Level.SEVERE, "Cannot perform output.", ex);
             // TODO add logging
             System.out.println("error");
+        }
+        catch (Exception e){
+            System.out.println("ops" + e);
+        }
+    }
+
+    private void loadFromFile(){
+        //deserialize file
+        try {
+            InputStream file = new FileInputStream(GameConstants.HIGHSCORE_PATH);
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInput input = new ObjectInputStream(buffer);
+
+            //deserialize the List
+            List<HighscoreEntry> highscoreEntries = (List<HighscoreEntry>) input.readObject();
+            //display its data
+            for (HighscoreEntry entry : highscoreEntries) {
+                System.out.println("Recovered " + entry.getName());
+            }
+            scoreList = (ArrayList<HighscoreEntry>) highscoreEntries;
+        }
+        catch(ClassNotFoundException ex){
+            System.out.println(ex);
+        }
+        catch(IOException ex){
+            System.out.println(ex);
         }
     }
 
