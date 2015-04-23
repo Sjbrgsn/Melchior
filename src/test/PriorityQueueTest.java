9e8ad6ac7ddce65c83fafaad2134e3d003f83bc1
Viktor Holmgren <vikho394@student.liu.se>
2015-04-23 13:38:23 +0200

    Restore test file, using junit was better

--- src/test/PriorityQueueTest.java
+++ src/test/PriorityQueueTest.java
@@ -1,9 +1,10 @@
 package test;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Test;
 import pathfinding.Location;
 import pathfinding.PriorityQueue;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Holmgr 2015-03-09
@@ -21,10 +22,10 @@ public class PriorityQueueTest {
         tester.enqueue(new Location(2, 2), 50);
 
         // Test
-        Assert.assertEquals(tester.dequeue(), new Location(2, 2), "PriorityQueue should give us Location(2, 2)");
+        assertEquals("PriorityQueue should give us Location(2, 2)", new Location(2, 2), tester.dequeue());
 
         tester.enqueue(new Location(3, 3), 75);
-        Assert.assertEquals(tester.dequeue(), new Location(3, 3), "PriorityQueue should give us Location(3, 3)");
-        Assert.assertEquals(tester.dequeue(), new Location(1, 1), "PriorityQueue should give us Location(1, 1)");
+        assertEquals("PriorityQueue should give us Location(3, 3)", new Location(3, 3), tester.dequeue());
+        assertEquals("PriorityQueue should give us Location(1, 1)", new Location(1, 1), tester.dequeue());
     }
 }
